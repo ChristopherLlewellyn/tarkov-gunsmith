22d674e06700fa96df9c65671516f954cdd567ce
Chris <christerface@gmail.com>
2019-10-06 19:08:35 +0100

    install and set up vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import { sync } from 'vuex-router-sync';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
@@ -13,5 +14,6 @@ sync(store, router);
 new Vue({
   router,
   store,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app');
