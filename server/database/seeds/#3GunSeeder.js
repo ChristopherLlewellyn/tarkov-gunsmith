b0f4abef9944fe15e3bdff98b8f088a938e43a89
Chris <christerface@gmail.com>
2020-03-02 17:56:41 +0000

    update seeder filepaths

--- server/database/seeds/#3GunSeeder.js
+++ server/database/seeds/#3GunSeeder.js
@@ -16,7 +16,7 @@ const XLSX = require('xlsx')
 
 class GunSeeder {
   async run () {
-    const workbook = XLSX.readFile(process.cwd() + '/Database/Seeds/seeds.xlsx')
+    const workbook = XLSX.readFile(process.cwd() + '/database/seeds/seeds.xlsx')
     let guns = XLSX.utils.sheet_to_json(workbook.Sheets['Guns']) //json object
     
     for (var row in guns) {
