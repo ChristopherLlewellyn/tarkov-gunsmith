472f88cdb2a8f15bc5ae92e90dd838ac07a762b5
Chris <christerface@gmail.com>
2020-03-30 23:46:58 +0100

    Ergo chip

---
+++ client/src/components/ErgonomicsChip.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-chip color="green">
+    <v-avatar left class="green darken-4">
+      <v-icon>mdi-hand</v-icon>
+    </v-avatar>
+    <h3 class="font-weight-medium">{{ value }}</h3>
+  </v-chip>
+</template>
+
+<script>
+  export default {
+    name: "chip",
+    props: {
+      value: Number
+    },
+  };
+
+</script>
