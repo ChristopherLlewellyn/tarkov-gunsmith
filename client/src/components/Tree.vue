af85674b58f548ac3aa99be9736942cb62617616
Chris <christerface@gmail.com>
2020-05-22 17:03:30 +0100

    Bugfix - don't show weapon icon if no icon exists

--- client/src/components/Tree.vue
+++ client/src/components/Tree.vue
@@ -7,11 +7,26 @@
             <v-card-title class="blue-grey darken-2 justify-center">
               <v-icon class="pr-2" large>mdi-pine-tree</v-icon>
               Build Tree for {{treeData.short_name ? treeData.short_name : 'Missing name'}}
-              <v-img class="ml-3" :src="treeData.img ? treeData.img : null" max-height="60" max-width="60" contain></v-img>
+              <v-img
+                v-if="treeData.img"
+                class="ml-3"
+                :src="treeData.img ? treeData.img : null"
+                max-height="60"
+                max-width="60"
+                contain
+              ></v-img>
             </v-card-title>
             <v-container fluid>
-              <node-tree-create v-if="treeType == 'create'" :node="treeData" :availableAttachments="availableAttachments"></node-tree-create>
-              <node-tree-edit v-if="treeType == 'edit'" :node="treeData" :availableAttachments="availableAttachments"></node-tree-edit>
+              <node-tree-create
+                v-if="treeType == 'create'"
+                :node="treeData"
+                :availableAttachments="availableAttachments"
+              ></node-tree-create>
+              <node-tree-edit
+                v-if="treeType == 'edit'"
+                :node="treeData"
+                :availableAttachments="availableAttachments"
+              ></node-tree-edit>
               <node-tree-view v-if="treeType == 'view'" :node="treeData"></node-tree-view>
             </v-container>
           </v-card>
@@ -22,31 +37,29 @@
 </template>
 
 <script>
-  // Import NodeTree types here
-  import NodeTreeCreate from './NodeTreeCreate';
-    import NodeTreeEdit from './NodeTreeEdit';
-  import NodeTreeView from './NodeTreeView';
+// Import NodeTree types here
+import NodeTreeCreate from "./NodeTreeCreate";
+import NodeTreeEdit from "./NodeTreeEdit";
+import NodeTreeView from "./NodeTreeView";
 
-  export default {
-    props: {
-      treeData: Object,
-      availableAttachments: Array,
-      selectedAttachments: Array,
-      treeType: String
-    },
-    components: {
-      NodeTreeCreate,
-      NodeTreeEdit,
-      NodeTreeView,
-    },
-  };
-
+export default {
+  props: {
+    treeData: Object,
+    availableAttachments: Array,
+    selectedAttachments: Array,
+    treeType: String
+  },
+  components: {
+    NodeTreeCreate,
+    NodeTreeEdit,
+    NodeTreeView
+  }
+};
 </script>
 
 <style>
-  .tree-list ul {
-    padding-left: 16px;
-    margin: 6px 0;
-  }
-
+.tree-list ul {
+  padding-left: 16px;
+  margin: 6px 0;
+}
 </style>
