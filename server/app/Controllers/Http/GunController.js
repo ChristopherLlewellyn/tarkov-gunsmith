b9252e6af437a5e5cd629e120031d45b36abdf55
Chris <christerface@gmail.com>
2020-05-25 18:41:37 +0100

    Order guns by short_name on API

--- server/app/Controllers/Http/GunController.js
+++ server/app/Controllers/Http/GunController.js
@@ -6,7 +6,11 @@ class GunController {
 
   // return all guns
   async index({ response }) {
-    const guns = await Gun.all()
+    const guns = await Gun
+      .query()
+      .select('*')
+      .orderBy('short_name', 'asc')
+      .fetch()
 
     response.status(200).json({
       message: 'Here is every gun',
