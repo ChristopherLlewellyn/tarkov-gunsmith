3fa0164882ebbbe7b1c781a69b5ec53b43f0b708
Chris <christerface@gmail.com>
2020-07-27 11:02:04 +0100

    Format camelCase value on type chips to Sentence Case

--- client/src/components/TypeChip.vue
+++ client/src/components/TypeChip.vue
@@ -5,7 +5,7 @@
         <v-avatar left class="red darken-4">
           <v-icon>mdi-axis</v-icon>
         </v-avatar>
-        <h3 class="font-weight-medium">{{ value }}</h3>
+        <h3 class="font-weight-medium">{{ formattedValue }}</h3>
       </v-chip>
     </template>
     <h3 class="font-weight-medium">Type</h3>
@@ -17,6 +17,20 @@ export default {
   name: "chip",
   props: {
     value: String
+  },
+
+  computed: {
+    formattedValue() {
+      if(this.value) return this.camelCaseToSentenceCase(this.value);
+    }
+  },
+
+  methods: {
+    camelCaseToSentenceCase(text) {
+      let result = text.replace( /([A-Z])/g, " $1" );
+      let finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+      return finalResult;
+    }
   }
 };
 </script>
