86d88dc7416d50b93456e88a55a5ef2035acbe02
Chris Llewellyn <chr.llewellyn@gmail.com>
2021-03-17 18:24:05 +0000

    remove debug messages

--- server/app/Services/GoogleAuthService.js
+++ server/app/Services/GoogleAuthService.js
@@ -7,19 +7,15 @@ const client = new OAuth2Client(CLIENT_ID)
 
 class GoogleAuthService {
   async verify(token) {
-    console.log(`CLIENT_ID: ${CLIENT_ID}`)
-    console.log(`token: ${token}`)
     try {
       const ticket = await client.verifyIdToken({
         idToken: token,
         audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       });
       const payload = ticket.getPayload()
-      console.log(`payload: ${payload}`)
 
       // Extract user details
       const email = payload['email']
-      console.log(`email: ${email}`)
 
       const user = { 
         email 
